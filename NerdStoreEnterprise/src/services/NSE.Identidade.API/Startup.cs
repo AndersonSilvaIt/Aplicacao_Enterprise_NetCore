05f666c11a516df256656ff6d602064665a10bf4
Anderson <anderson@prodwin.com.br>
2020-09-17 22:02:03 -0300

    Commit Inicial - Api Login
    Implementando o Setup básico da API de login, usando o básico do Swagger para documentação da API.

---
+++ NerdStoreEnterprise/src/services/NSE.Identidade.API/Startup.cs
@@ -0,0 +1,71 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+using NSE.Identidade.API.Data;
+using System;
+
+namespace NSE.Identidade.API
+{
+	public class Startup
+	{
+		public Startup(IConfiguration configuration)
+		{
+			Configuration = configuration;
+		}
+
+		public IConfiguration Configuration { get; }
+
+		public void ConfigureServices(IServiceCollection services)
+		{
+			services.AddDbContext<ApplicationDbContext>(options =>
+				options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+			services.AddDefaultIdentity<IdentityUser>()
+				.AddRoles<IdentityRole>()
+				.AddEntityFrameworkStores<ApplicationDbContext>()
+				.AddDefaultTokenProviders();
+
+			services.AddControllers();
+
+			services.AddSwaggerGen(c => {
+				c.SwaggerDoc("v1", new OpenApiInfo() {
+					Title = "Nerdstore Enterprise Identity API",
+					Description = "Esta API faz parte do custo Asp.Net Core Enterprise Application.",
+					Contact = new OpenApiContact() { Name = "Anderson Enterprise", Email = "anderson.silvait@outlook.com.br"},
+					License = new OpenApiLicense() {  Name = "MIT", Url = new Uri("https://opensource.org/licenses/MIT")}
+				});
+			});
+		}
+
+		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+		{
+			app.UseSwagger();
+			app.UseSwaggerUI(c => {
+				c.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+			});
+
+			if (env.IsDevelopment())
+			{
+				app.UseDeveloperExceptionPage();
+			}
+
+			app.UseHttpsRedirection();
+
+			app.UseRouting();
+			
+			app.UseAuthentication();
+			app.UseAuthorization();
+
+			app.UseEndpoints(endpoints =>
+			{
+				endpoints.MapControllers();
+			});
+
+		}
+	}
+}
