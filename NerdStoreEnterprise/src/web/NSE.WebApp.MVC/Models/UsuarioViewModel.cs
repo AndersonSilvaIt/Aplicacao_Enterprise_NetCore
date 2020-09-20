da3d215afa89a27bd99f214645a6998af7595d08
Anderson <anderson@prodwin.com.br>
2020-09-20 13:16:14 -0300

    Implementado a comunicação entre a web e a API
    Implementado a comunicação entre a web e a API de autenticação.

--- NerdStoreEnterprise/src/web/NSE.WebApp.MVC/Models/UsuarioViewModel.cs
+++ NerdStoreEnterprise/src/web/NSE.WebApp.MVC/Models/UsuarioViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace NSE.WebApp.MVC.Models
 {
@@ -25,4 +26,25 @@ namespace NSE.WebApp.MVC.Models
 		//[StringLength(100, ErrorMessage = "O campo {0} precisa ter entre {2} e {1] caracteres", MinimumLength = 0)]
 		public string Senha { get; set; }
 	}
+
+	public class UsuarioRespostaLogin
+	{
+		public string AccessToken { get; set; }
+		public double ExpiresIn { get; set; }
+		public UsuarioToken UsuarioToken { get; set; }
+	}
+
+	public class UsuarioToken
+	{
+		public string Id { get; set; }
+		public string Email { get; set; }
+		public IEnumerable<UsuarioClaim> Claims { get; set; }
+	}
+
+	public class UsuarioClaim
+	{
+		public string Value { get; set; }
+		public string Type { get; set; }
+	}
+
 }
---
+++ NerdStoreEnterprise/src/web/NSE.WebApp.MVC/Services/AutenticacaoService.cs
@@ -0,0 +1,55 @@
+﻿using NSE.WebApp.MVC.Models;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace NSE.WebApp.MVC.Services
+{
+	public class AutenticacaoService : IAutenticacaoService
+	{
+		private readonly HttpClient _httpClient;
+		public AutenticacaoService(HttpClient httpClient)
+		{
+			_httpClient = httpClient;
+		}
+
+		public async Task<UsuarioRespostaLogin> Login(UsuarioLogin usuarioLogin)
+		{
+			var loginContent = new StringContent(
+				JsonSerializer.Serialize(usuarioLogin),
+				Encoding.UTF8, "application/json");
+
+			var response = await _httpClient.PostAsync("https://localhost:44365/api/identidade/autenticar", loginContent);
+
+			//var teste = response.Content.ReadAsStringAsync();
+
+			var options = new JsonSerializerOptions
+			{
+				PropertyNameCaseInsensitive = true,
+			};
+
+			return JsonSerializer.Deserialize<UsuarioRespostaLogin>(await response.Content.ReadAsStringAsync(), options);
+		}
+
+		public async Task<UsuarioRespostaLogin> Registro(UsuarioRegistro usuarioRegistro)
+		{
+			var registroContent = new StringContent(
+					JsonSerializer.Serialize(usuarioRegistro),
+					Encoding.UTF8, "application/json");
+
+			var response = await _httpClient.PostAsync("https://localhost:44365/api/identidade/nova-conta", registroContent);
+
+			//var teste = await response.Content.ReadAsStringAsync();
+			//Esse trecho de código serve para realizar a Serialização, isso no System.Text, pois o Objeto contem as propriedades 
+			//com letras maiúsculas, e os dados que vem da API estão todas em letras minúsculas.
+			// isso era tratado no NewtonSoft
+			var options = new JsonSerializerOptions
+			{
+				PropertyNameCaseInsensitive = true,
+			};
+
+			return JsonSerializer.Deserialize<UsuarioRespostaLogin>(await response.Content.ReadAsStringAsync(), options);
+		}
+	}
+}
