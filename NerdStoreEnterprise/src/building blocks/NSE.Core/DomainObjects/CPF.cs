2cc027cbdbaedd0a57e7d039f3c010793f3a9522
Anderson <anderson@prodwin.com.br>
2020-10-06 17:52:40 -0300

    Objetos de Valor
    Objetos de Valor CPF / Email com suas devidas validações.

---
+++ NerdStoreEnterprise/src/building blocks/NSE.Core/DomainObjects/CPF.cs
@@ -0,0 +1,65 @@
+﻿using NSE.Core.Utils;
+
+namespace NSE.Core.DomainObjects
+{
+	public class CPF
+	{
+		public const int CpfMaxLength = 11;
+		public string Numero { get; private set; }
+
+		// Construtor do EntityFramework
+		protected CPF() { }
+
+		public CPF(string numero)
+		{
+			//ex
+			if (!CPF.Validar(numero)) throw new DomainException("CPF inválido");
+
+			Numero = numero;
+		}
+
+		public static bool Validar(string cpf)
+		{
+			cpf = cpf.ApenasNumeros(cpf);
+
+
+			if (cpf.Length > 11)
+				return false;
+
+			while (cpf.Length != 11)
+				cpf = '0' + cpf;
+
+			var igual = true;
+
+			for (int i = 0; i < 11; i++)
+				if (cpf[i] != cpf[0])
+					igual = false;
+
+			if (igual || cpf == "12345678909")
+				return false;
+
+			var numeros = new int[11];
+
+			for (int i = 0; i < 11; i++)
+				numeros[i] = int.Parse(cpf[i].ToString());
+
+			var soma = 0;
+			for (int i = 0; i < 9; i++)
+				soma += (10 - i) * numeros[i];
+
+			var resultado = soma % 11;
+
+			if(resultado == 1 || resultado == 0)
+			{
+				if (numeros[10] != 0)
+					return false;
+			}
+			else if(numeros[10] != 11 - resultado)
+				return false;
+
+			return true;
+		}
+
+	}
+
+}
---
+++ NerdStoreEnterprise/src/services/NSE.Cliente.API/Models/Cliente.cs
@@ -0,0 +1,39 @@
+﻿using NSE.Core.DomainObjects;
+using System;
+
+namespace NSE.Cliente.API.Models
+{
+	public class Cliente : Entity, IAggregateRoot
+	{
+		public string Nome { get; private set; }
+		public Email Email { get; private set; }
+		public CPF CPF { get; private set; }
+		public bool Excluido { get; private set; }
+		public Endereco Endereco { get; private set; }
+
+		//EF Relation
+		protected Cliente() {}
+
+		public Cliente(Guid id, string nome, string email, string cpf)
+		{
+			Id = id;
+			Nome = nome;
+			Email = new Email(email);
+			CPF = new CPF(cpf);
+			Excluido = false;
+		}
+
+		public void AtribuirEndereco(Endereco endereco)
+		{
+			Endereco = endereco;
+		}
+
+		public void TrocarEmail(string email)
+		{
+			Email = new Email(email);
+		}
+
+	}
+}
+
+
