ed9727aa99a82020dfb2c89a9880b465c8a9ea10
Anderson <anderson@prodwin.com.br>
2020-12-17 18:19:09 -0300

    Implementando a arquitetura da API de Pedidos
    Implementando a arquitetura da API de Pedidos

---
+++ NerdStoreEnterprise/src/services/NSE.Pedidos.Infra/Data/Repository/VoucherRepository.cs
@@ -0,0 +1,22 @@
+﻿using NSE.Core.Data;
+using NSE.Pedidos.Domain.Vouchers;
+
+namespace NSE.Pedidos.Infra.Data.Repository
+{
+	//public class VoucherRepository : IVoucherRepository
+	//{
+	//	private readonly PedidosContext _context;
+	//
+	//	public VoucherRepository(PedidosContext _context)
+	//	{
+	//		_context = _context;
+	//	}
+	//
+	//	public IUnitOfWork UnitOfWork => _context;
+	//
+	//	public void Dispose()
+	//	{
+	//		_context.Dispose();
+	//	}
+	//}
+}	//
