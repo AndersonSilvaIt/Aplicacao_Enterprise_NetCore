2996fbbd62fa41e2ddff0c0920c9f921cc306212
Anderson <anderson@prodwin.com.br>
2020-12-25 18:51:42 -0300

    Implementado a solicitação de Pagamento
    Implementado a solicitação de Pagamento

--- NerdStoreEnterprise/src/services/NSE.Pedidos.API/Configuration/MessageBusConfig.cs
+++ NerdStoreEnterprise/src/services/NSE.Pedidos.API/Configuration/MessageBusConfig.cs
@@ -10,7 +10,9 @@ namespace NSE.Pedidos.API.Configuration
         public static void AddMessageBusConfiguration(this IServiceCollection services,
             IConfiguration configuration)
         {
-            services.AddMessageBus(configuration.GetMessageQueueConnection("MessageBus"));
+            services.AddMessageBus(configuration.GetMessageQueueConnection("MessageBus"))
+                        .AddHostedService<PedidoOrquestradorIntegrationHandler>()
+                        .AddHostedService<PedidoIntegrationHandler>();
         }
     }
 }
