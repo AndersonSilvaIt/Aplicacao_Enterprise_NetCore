5ec384ea8421f7a08eca112d1640e3d2107cb0d2
Anderson <anderson@prodwin.com.br>
2020-12-26 17:42:49 -0300

    Baixando o Pedido do Estoque
    Baixando o Pedido do Estoque

--- NerdStoreEnterprise/src/services/NSE.Pedidos.API/Services/PedidoOrquestradorIntegrationHandler.cs
+++ NerdStoreEnterprise/src/services/NSE.Pedidos.API/Services/PedidoOrquestradorIntegrationHandler.cs
@@ -1,6 +1,11 @@
-﻿using Microsoft.Extensions.Hosting;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using NSE.Core.Messages.Integration;
+using NSE.MessageBus;
+using NSE.Pedidos.API.Application.Queries;
 using System;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -10,10 +15,13 @@ namespace NSE.Pedidos.API.Services
 	{
 		private readonly ILogger<PedidoOrquestradorIntegrationHandler> _logger;
 		private Timer _timer;
+		private readonly IServiceProvider _serviceProvider;
 
-		public PedidoOrquestradorIntegrationHandler(ILogger<PedidoOrquestradorIntegrationHandler> logger)
+		public PedidoOrquestradorIntegrationHandler(ILogger<PedidoOrquestradorIntegrationHandler> logger,
+			IServiceProvider serviceProvider)
 		{
 			_logger = logger;
+			_serviceProvider = serviceProvider;
 		}
 
 		public Task StartAsync(CancellationToken cancellationToken)
@@ -28,7 +36,24 @@ namespace NSE.Pedidos.API.Services
 
 		private async void ProcessarPedidos(object state)
 		{
-			_logger.LogInformation("Processando pedidos.");
+			using (var scope = _serviceProvider.CreateScope())
+			{
+				var pedidoQueries = scope.ServiceProvider.GetRequiredService<IPedidoQueries>();
+				var pedido = await pedidoQueries.ObterPedidosAutorizados();
+
+				if (pedido == null) return;
+
+				var bus = scope.ServiceProvider.GetRequiredService<IMessageBus>();
+
+				if (pedido == null) return;
+
+				var pedidoAutorizado = new PedidoAutorizadoIntegrationEvent(pedido.ClienteId, pedido.Id,
+					pedido.PedidoItems.ToDictionary(p => p.ProdutoId, p => p.Quantidade));
+
+				await bus.PublishAsync(pedidoAutorizado);
+
+				_logger.LogInformation($"Pedido ID: {pedido.Id} foi encaminhado para baixa no estoque.");
+			}
 		}
 
 		public Task StopAsync(CancellationToken cancellationToken)
