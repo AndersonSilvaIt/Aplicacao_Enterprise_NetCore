8497621400f4312b681872cae337e2defe827643
Anderson <anderson@prodwin.com.br>
2020-12-28 13:51:00 -0300

    Refresh Token na API de identidade
    Refresh Token na API de identidade

---
+++ NerdStoreEnterprise/src/services/NSE.Identidade.API/Model/RefreshToken.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace NSE.Identidade.API.Model
+{
+	public class RefreshToken
+	{
+		public RefreshToken()
+		{
+			Id = Guid.NewGuid();
+			Token = Guid.NewGuid();
+		}
+
+		public Guid Id { get; set; }
+		public Guid Token { get; set; }
+
+		public string UserName { get; set; }
+		public DateTime ExpirationDate { get; set; }
+	}
+}
