f9c4565ffbef1a6277144f8801bdfa853b4c29ae
Anderson <anderson@prodwin.com.br>
2020-12-28 15:26:46 -0300

    Refresh Token no MVC
    Refresh Token no MVC

--- NerdStoreEnterprise/src/web/NSE.WebApp.MVC/Extensions/ExceptionMiddleware.cs
+++ NerdStoreEnterprise/src/web/NSE.WebApp.MVC/Extensions/ExceptionMiddleware.cs
@@ -1,6 +1,7 @@
 ﻿using System.Net;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
+using NSE.WebApp.MVC.Services;
 using Polly.CircuitBreaker;
 using Refit;
 
@@ -9,14 +10,17 @@ namespace NSE.WebApp.MVC.Extensions
     public class ExceptionMiddleware
     {
         private readonly RequestDelegate _next;
+        private static IAutenticacaoService _autenticacaoService;
 
         public ExceptionMiddleware(RequestDelegate next)
         {
             _next = next;
         }
 
-        public async Task InvokeAsync(HttpContext httpContext)
+        public async Task InvokeAsync(HttpContext httpContext, IAutenticacaoService autenticacaoService)
         {
+            _autenticacaoService = autenticacaoService;
+
             try
             {
                 await _next(httpContext);
@@ -43,6 +47,17 @@ namespace NSE.WebApp.MVC.Extensions
         {
             if (statusCode == HttpStatusCode.Unauthorized)
             {
+                //Token expirado, irá tentar renovar o Token usando o Refresh
+                if (_autenticacaoService.TokenExpirado())
+                {
+                    if (_autenticacaoService.RefreshTokenValido().Result)
+                    {
+                        context.Response.Redirect(context.Request.Path);
+                        return;
+                    }
+                }
+                // caso não conseguir realizar o Refresh, irá realizar o logout na aplicação
+                _autenticacaoService.Logout();
                 context.Response.Redirect($"/login?ReturnUrl={context.Request.Path}");
                 return;
             }
--- NerdStoreEnterprise/src/web/NSE.WebApp.MVC/Services/IAutenticacaoService.cs
+++ NerdStoreEnterprise/src/web/NSE.WebApp.MVC/Services/IAutenticacaoService.cs
@@ -6,7 +6,10 @@ namespace NSE.WebApp.MVC.Services
     public interface IAutenticacaoService
     {
         Task<UsuarioRespostaLogin> Login(UsuarioLogin usuarioLogin);
-
         Task<UsuarioRespostaLogin> Registro(UsuarioRegistro usuarioRegistro);
+        Task RealizarLogin(UsuarioRespostaLogin resposta);
+        Task Logout();
+        bool TokenExpirado();
+        Task<bool> RefreshTokenValido();
     }
 }
